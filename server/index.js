31966b412d076ab5d44b17a93b8040bcad2f1513
nakietta <ntkgr9@gmail.com>
2025-11-21 23:21:49 +0800

    修复：FC3.0 HTTP函数入口格式

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-// 阿里云函数计算入口文件
-const serverless = require('serverless-http');
+// 阿里云函数计算3.0入口文件
 const app = require('./server');
 
-// 导出函数计算handler
-module.exports.handler = serverless(app);
+// 🔥 FC3.0 HTTP函数入口
+module.exports.handler = async (event, context) => {
+  // 转换 FC 事件为 Express 请求
+  const serverless = require('serverless-http');
+  const handler = serverless(app);
+  return await handler(event, context);
+};
